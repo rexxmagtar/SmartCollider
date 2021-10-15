83c5f2ae8aea458eb783b8ab963d860fd0f735a7
Ivan <rexx.magtar@gmail.com>
2021-10-15 16:31:37 +0300

    code added

---
+++ TriggerZone.cs
@@ -0,0 +1,156 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace com.pigsels.BubbleTrouble
+{
+    /// <summary>
+    /// Determines collision zone. Wrappers collision enter and leave events.
+    /// Used as a detector by <see cref="ZoneTriggerGameTrigger"/>.
+    /// </summary>
+    [RequireComponent(typeof(Collider2D))]
+    public class TriggerZone : MonoBehaviour
+    {
+        public delegate void TriggerHandler(TriggerZone triggerZone, Collider2D collider1, Collider2D collider2, MonoBehaviour collidedMonoBehaviour);
+
+        public event TriggerHandler OnTriggerExit;
+        public event TriggerHandler OnTriggerEnter;
+
+        /// <summary>
+        /// Gameobjects collider. Determines collision zone borders
+        /// </summary>
+        private Collider2D objectCollider;
+
+        /// <summary>
+        /// Dictionary of objects inside trigger zone and their number of colliders which are inside trigger zone too.
+        /// </summary>
+        private Dictionary<MonoBehaviour, int> objectsInsideZone = new Dictionary<MonoBehaviour, int>();
+
+        private void Awake()
+        {
+            // Make sure this GameObject won't affect Trigger colliders.
+            gameObject.tag = "Detector";
+
+            objectCollider = gameObject.GetComponent<Collider2D>();
+
+            if (!objectCollider.isTrigger)
+            {
+                Debug.LogWarning($"Collider of {objectCollider.gameObject} is not set to trigger mode. Collisions may be ignored");
+            }
+        }
+
+        void OnTriggerEnter2D(Collider2D other)
+        {
+            //Debug.Log($"On trigger enter for : {other.gameObject}. Parent: {other.transform.parent?.name}");
+
+            // If a GameEntity has multiple children with their own colliders
+            // then in case when two or more of children enter zone's collider during same fixUpdate then methods OnTriggerEnter2D and OnTriggerExit2D may trigger in wrong order.
+            // For instance if GameEntity has 5 children and all of them get inside collider during the same FixedUpdate then the sequence of  OnTriggerEnter2D and  OnTriggerExit2D events
+            // may be the following: OnTriggerEnter2D for child_1 (child_1 gets destroyed or deactivated during this call) -> OnTriggerExit2D for child_1 -> 
+            // -> OnTriggerExit2D for child_2,child_3, ... child_5 -> OnTriggerEnter2D for child_2,child_3, ... child_5.
+            // Subsequently, this misorder causes misorder in object registration and unregistration in zone order.
+            // Extra explanations can be found in comments for this task https://trello.com/c/yRvGhiJZ/300-triggerzonegametrigger-add-invisible-trigger-zones
+
+            // The two following checks solve the issue described above:
+
+            // Checking if the object is active.
+            // If it's not then it means that the object has already been deactivated and left zone (OnTriggerExit2D was already called).
+            if (!other.gameObject.activeInHierarchy)
+            {
+                // Debug.LogWarning("Inactive object caught");
+                return;
+            }
+
+            // Checking if the objects' collider component is still enabled.
+            // If it's not then it means that the object  has already been destroyed and left zone (OnTriggerExit2D was already called).
+            if (!other.isActiveAndEnabled)
+            {
+                // Debug.LogWarning("Destroyed object caught");
+                return;
+            }
+
+            if (!FilterCollision(other, out MonoBehaviour collidedObject))
+            {
+                return;
+            }
+
+            // Debug.Log($"Filtered enter GameObject : {collidedObject.gameObject}. Parent: {other.transform.parent?.name}.");
+
+            if (objectsInsideZone.ContainsKey(collidedObject)) // Has object already entered?
+            {
+                objectsInsideZone[collidedObject]++; // Just adding new collider of object inside zone trigger. 
+                return;
+            }
+
+            //Adding new object to the zone trigger.
+            objectsInsideZone.Add(collidedObject, 1);
+
+            OnTriggerEnter?.Invoke(this, objectCollider, other, collidedObject);
+        }
+
+        void OnTriggerExit2D(Collider2D other)
+        {
+            //Debug.Log($"On trigger exit for : {other.gameObject}. Parent: {other.transform.parent?.name}");
+
+            if (!FilterCollision(other, out MonoBehaviour collidedObject))
+            {
+                return;
+            }
+
+            // Debug.Log($"Filtered exit GameObject : {collidedObject.gameObject}. Parent: {other.transform.parent?.name}.");
+
+            if (!objectsInsideZone.ContainsKey(collidedObject)) //Has object already left the trigger zone? This could have been caused by misorder of trigger OnEnter and OnExit calls. 
+            {
+                //Doing nothing cause object already left.
+                return;
+            }
+
+            //Removing objects collider that left trigger zone.
+            objectsInsideZone[collidedObject]--;
+
+            if (objectsInsideZone[collidedObject] > 0) // Are any of object's colliders still left in a trigger zone. 
+            {
+                //Doing nothing cause there a still some colliders.
+                return;
+            }
+
+            //Removing object from trigger zone.
+            objectsInsideZone.Remove(collidedObject);
+
+            OnTriggerExit?.Invoke(this, objectCollider, other, collidedObject);
+        }
+
+        /// <summary>
+        /// Does base filtering. Checks that collided object doesn't have specified tags.
+        /// Also checks if collided object is a <see cref="Wagon"/> or a <see cref="GameEntity"/>.
+        /// </summary>
+        /// <param name="other">Collided object's collider</param>
+        /// <param name="collidedObject"><see cref="GameEntity"/> or a <see cref="Wagon"/> component of filtered gameobject or null if gameobject didn't pass the filter.</param>
+        /// <returns>Result of filtering. True if filter has been passed. False over-wise</returns>
+        private bool FilterCollision(Collider2D other, out MonoBehaviour collidedObject)
+        {
+            collidedObject = null;
+
+            if (other.gameObject.CompareTag("Detector") || other.gameObject.CompareTag("Droplet"))
+            {
+                return false;
+            }
+
+            MonoBehaviour collidedMonoBehaviour = other.gameObject.transform.root.gameObject.GetComponent<Wagon>();
+
+            if (collidedMonoBehaviour == null)
+            {
+                collidedMonoBehaviour = other.gameObject.transform.root.gameObject.GetComponent<GameEntity>();
+            }
+
+            if (collidedMonoBehaviour == null)
+            {
+                return false;
+            }
+
+            collidedObject = collidedMonoBehaviour;
+
+            return true;
+        }
+    }
+}
---
+++ ZoneTriggerGameTrigger.cs
@@ -0,0 +1,238 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using com.pigsels.tools;
+using UnityEngine;
+
+namespace com.pigsels.BubbleTrouble
+{
+    /// <summary>
+    /// Triggers when wagon collides with scenes <see cref="TriggerZone"/>.
+    /// Allows filtering for collided <see cref="TriggerZone"/> and collided object.
+    /// </summary>
+    [EditorGroup("Game Level")]
+    public class ZoneTriggerGameTrigger : GameTriggerSO
+    {
+#region Serialized fields
+
+        /// <summary>
+        /// Determines if filtering for collided zone must be applied.
+        /// </summary>
+        [Tooltip("Should objects be filetered by zone name when collided?")]
+        public FilterMode filterTriggersZoneName = FilterMode.Any;
+
+        /// <summary>
+        /// Regex pattern used for zone name filtering if <see cref="filterTriggersZoneName"/> is true.
+        /// </summary>
+        [ShowIf("filterTriggersZoneName != Any"), ChangeNesting(1)]
+        [Icon("warning", "Pattern to match zone name. This is the regular expression string. Click the icon for details.", null, RegexHintURL)]
+        public string triggerZoneNamePattern;
+
+        /// <summary>
+        /// Determines if filtering for collided object type must be applied.
+        /// </summary>
+        [Tooltip("Should objects be filetered by their type when collided?")]
+        public FilterMode filterObjectType = FilterMode.Any;
+
+        /// <summary>
+        ///Determines if collisions with wagon must be detected. Used in case when <see cref="filterObjectType"/> is true.
+        /// </summary>
+        [ShowIf("filterObjectType != Any"), ChangeNesting(1)]
+        public bool reactToWagonCollision;
+
+        /// <summary>
+        ///Determines if collisions with specified <see cref="GameEntity"/> must be detected. Used in case when <see cref="filterObjectType"/> is true.
+        /// </summary>
+        [ShowIf("filterObjectType != Any"), ChangeNesting(1)]
+        public GameEntityList EntityList;
+
+        /// <summary>
+        /// Determines if filtering for collided object name must be applied.
+        /// </summary>
+        [Tooltip("Should objects be filetered by their name when collided?")]
+        public FilterMode filterObjectName = FilterMode.Any;
+
+        /// <summary>
+        /// Regex pattern used for object name filtering if <see cref="filterObjectName"/> is true.
+        /// </summary>
+        [ShowIf("filterObjectName != Any"), ChangeNesting(1)]
+        [Icon("warning", "Pattern to match gameobject name. This is the regular expression string. Click the icon for details.", null, RegexHintURL)]
+        public string gameObjectNamePattern;
+
+        /// <summary>
+        /// Determines if trigger must be fired on zones enter events.
+        /// </summary>
+        [Tooltip("Should the trigger be activated when an object enters the trigger zone.")]
+        public bool triggerOnZoneEnter;
+
+        /// <summary>
+        /// Determines if trigger must be fired on zones leave events.
+        /// </summary>
+        [Tooltip("Should the trigger be activated when an object leaves the trigger zone.")]
+        public bool triggerOnZoneLeave;
+
+        /// <summary>
+        /// Determines if all filtered objects must be treated as one.
+        /// In this case OnZoneExit will trigger only when zone is empty and OnZoneEnter will trigger only if the first object has entered.
+        /// </summary>
+        [Tooltip("Whether all filtered objects must be treated as one. When enabled, ZoneEnter will be triggered only when first object enters the zone and the last leaves it.")]
+        public bool treatObjectsAsOne = false;
+
+#endregion
+
+
+#region Dynamic fields
+
+        private List<TriggerZone> TriggerZones;
+
+        /// <summary>
+        /// Number of filtered objects inside the zone. Works in pair with <see cref="treatObjectsAsOne"/>
+        /// </summary>
+        private int zoneObjectsCount = 0;
+
+#endregion
+
+
+        public override void Init()
+        {
+            base.Init();
+
+            //TriggerZones = GameObject.FindObjectsOfType<TriggerZone>();
+            TriggerZones = HelperTools.GetSceneBehaviors<TriggerZone>(false);
+
+            foreach (var triggerZone in TriggerZones)
+            {
+                triggerZone.OnTriggerEnter += HandleTriggerEnter;
+                triggerZone.OnTriggerExit += HandleTriggerExit;
+            }
+        }
+
+        public override void Deinit()
+        {
+            foreach (var triggerZone in TriggerZones)
+            {
+                triggerZone.OnTriggerEnter -= HandleTriggerEnter;
+                triggerZone.OnTriggerExit -= HandleTriggerExit;
+            }
+        }
+
+        /// <summary>
+        /// Handler of the collision enter event
+        /// </summary>
+        /// <param name="triggerZone">collided zone</param>
+        /// <param name="collider1">collided zone collider</param>
+        /// <param name="collider2">collided object collider</param>
+        private void HandleTriggerEnter(TriggerZone triggerZone, Collider2D collider1, Collider2D collider2, MonoBehaviour collidedMonoBehaviour)
+        {
+            //Debug.Log("Handle trigger enter");
+
+            if (DoFilter(triggerZone, collidedMonoBehaviour))
+            {
+                zoneObjectsCount++;
+
+                if (triggerOnZoneEnter)
+                {
+                    if (!treatObjectsAsOne || zoneObjectsCount == 1)
+                    {
+                        Trigger(collidedMonoBehaviour);
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Handler of the collision leave event
+        /// </summary>
+        /// <param name="triggerZone">collided zone</param>
+        /// <param name="collider1">collided zone collider</param>
+        /// <param name="collider2">collided object collider</param>
+        private void HandleTriggerExit(TriggerZone triggerZone, Collider2D collider1, Collider2D collider2, MonoBehaviour collidedMonoBehaviour)
+        {
+            if (DoFilter(triggerZone, collidedMonoBehaviour))
+            {
+                zoneObjectsCount--;
+
+                if (triggerOnZoneLeave)
+                {
+                    if (!treatObjectsAsOne || zoneObjectsCount == 0)
+                    {
+                        Trigger(collidedMonoBehaviour);
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Does required filtering for collided object and trigger zone.
+        /// </summary>
+        /// <param name="triggerZone"></param>
+        /// <param name="collidedMonoBehaviour"></param>
+        /// <returns>True if filter control succeeds. False over-wise</returns>
+        private bool DoFilter(TriggerZone triggerZone, MonoBehaviour collidedMonoBehaviour)
+        {
+            if (filterTriggersZoneName != FilterMode.Any)
+            {
+                Regex zoneNameRegex = new Regex(triggerZoneNamePattern);
+
+                if ((filterTriggersZoneName == FilterMode.Require && !zoneNameRegex.IsMatch(triggerZone.gameObject.name))
+                    || (filterTriggersZoneName == FilterMode.Exclude && zoneNameRegex.IsMatch(triggerZone.gameObject.name)))
+                {
+                    return false;
+                }
+            }
+
+            if (filterObjectType != FilterMode.Any)
+            {
+                if (collidedMonoBehaviour is Wagon)
+                {
+                    if ((filterObjectType == FilterMode.Require && !reactToWagonCollision)
+                        || (filterObjectType == FilterMode.Exclude && reactToWagonCollision))
+                    {
+                        return false;
+                    }
+                }
+                else // at this point collided monobeh is a GameEntity cause ZoneTrigger fires collisions only with either Wagon or GameEntity.
+                {
+                    if ((filterObjectType == FilterMode.Require && !EntityList.selectedEntities.Contains(collidedMonoBehaviour.GetType().AssemblyQualifiedName))
+                        || (filterObjectType == FilterMode.Exclude && EntityList.selectedEntities.Contains(collidedMonoBehaviour.GetType().AssemblyQualifiedName)))
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            if (filterObjectName != FilterMode.Any)
+            {
+                Regex gameobjectNameRegex = new Regex(gameObjectNamePattern);
+
+                if ((filterObjectName == FilterMode.Require && !gameobjectNameRegex.IsMatch(collidedMonoBehaviour.gameObject.name))
+                    || (filterObjectName == FilterMode.Exclude && gameobjectNameRegex.IsMatch(collidedMonoBehaviour.gameObject.name)))
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        /// <summary>
+        /// Fires trigger.
+        /// Sets event data such as collided object position and collided entities.
+        /// </summary>
+        /// <param name="collidedCollider"></param>
+        private void Trigger(MonoBehaviour collidedObject)
+        {
+            //Debug.Log($"Doing TRIGGER for collider {collidedObject.gameObject}");
+
+            SetEventPoint(collidedObject.gameObject.transform.position);
+
+            if (collidedObject is GameEntity)
+            {
+                SetEventGameEntities(new List<GameEntity>() {(GameEntity)collidedObject});
+            }
+
+            Activate();
+        }
+    }
+}
